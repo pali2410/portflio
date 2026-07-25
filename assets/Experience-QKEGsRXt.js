ab50d5e050864f6e3911d8f0896ecc0627bb2a67
estatelandscapers007 <estatelandscapers007@gmail.com>
2026-07-25 18:32:03 +0530

    Fix iOS WebKit GLSL float precision for Troika 3D text and center 2D mobile HTML overlay modals in viewport

--- assets/Experience-QKEGsRXt.js
+++ assets/Experience-QKEGsRXt.js
@@ -583,7 +583,7 @@ float troikaGlyphUvToSdfValue(vec2 glyphUV) {
   vec2 textureUV = mix(vTroikaTextureUVBounds.xy, vTroikaTextureUVBounds.zw, glyphUV);
   vec4 rgba = texture2D(uTroikaSDFTexture, textureUV);
   float ch = floor(vTroikaTextureChannel + 0.5); //NOTE: can't use round() in WebGL1
-  return ch == 0.0 ? rgba.r : ch == 1.0 ? rgba.g : ch == 2.0 ? rgba.b : rgba.a;
+  return ch < 0.5 ? rgba.r : ch < 1.5 ? rgba.g : ch < 2.5 ? rgba.b : rgba.a;
 }
 
 float troikaGlyphUvToDistance(vec2 uv) {
